11a50e5c4f2dff9f4a6fd4ca26fa112a532df2be
leilanazarli <leyla.nezerli.2002@gmail.com>
2023-06-18 22:19:51 +0400

    chance login page

--- assets/js/personnel.js
+++ assets/js/personnel.js
@@ -1,6 +1,7 @@
 let tbody=document.querySelector("tbody")
 let BASE_URL="http://localhost:8000"
 
+
 async function getTablePersonnel(){
 tbody.innerHTML=""
     let res=await axios(`${BASE_URL}/users`)
@@ -19,36 +20,56 @@ tbody.innerHTML=""
         <td class="action">
         <a href=""><i class="fa-sharp fa-solid fa-trash"></i></a>
         <a href=""><i class="fa-regular fa-pen-to-square"></i></a>
-        <a href="" onclick="deleteBtn({elementid})"><i class="fa-solid fa-xmark"></i></a>
-        <a href=""><i class="fa-solid fa-eye"></i></a>
+        <a href="" onclick="deleteBtn(${element.id})"><i class="fa-solid fa-xmark"></i></a>
+        <a id="myBtn" onclick="detailBtn(${element.id})" ><i class="fa-solid fa-eye"></i></a>
+                  <div id="myModal" class="modal">
+                    <div class="modal-content">
+                      <span class="close" onclick="closeBtn(${element.id})" >&times;</span>
+                      <img src="./assets/img/about-img/${element.image}" alt="" width="350px" >
+                    </div>
+                  </div>
         </td>
         </tr>
         `
     });
 }
-// getTablePersonnel()
-// Get the modal
-var modal = document.getElementById("myModal");
+getTablePersonnel()
+
+// var modal = document.querySelector("#myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// var btn = document.querySelector("#myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.querySelector(".close");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// btn.addEventListener("click" ,function() {
+//     modal.style.display = "block";
+// })
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.addEventListener("click" ,function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// })
+
+
+async function detailBtn(){
+    modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+async function closeBtn(){
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.addEventListener("click" ,function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+})
